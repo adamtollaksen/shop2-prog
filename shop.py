16682d3b26169f92195f672fc38199935dcff69a
adamtollaksen <adam.tollaksen@elev.ga.ntig.se>
2022-03-11 08:42:02 +0100

    update

--- shop.py
+++ shop.py
@@ -1,40 +1,53 @@
-def Main():
-    input("Welcome to the shop, would you like to enter?(yes/no) ")
-    if "yes":
-        print("Welcome!")
-        money = input("How much money do you have on you right now?(number only) ")
-        print("Your current balance is: ", money)
-    
-    print("Here are our current items in the shop. Is there anything you're looking to buy?(answer with the name of the item you want to buy): ")
+from tracemalloc import stop
 
-    elif "no":
-        print("Have a good day!")
 
-"""
+
 class items: 
     def __init__(self, name, price): 
         self.name = name 
-        self.roll = price
+        self.price = price
           
 list = [] 
   
-list.append( items('banan', 1) )
-list.append( items('äpple', 2) )
-list.append( items('godisbit', 5) )
-list.append( items('monster', 20) )
-list.append( items('köttbullar', 45) )
-list.append( items('oxfile', 60) )
-list.append( items('bilmotor', 100 000) )
-list.append( items('båtmotor', 200 000) )
-list.append( items('dörr', 1 500) )
-list.append( items('allhjul', 800) )
-list.append( items('skärm', 3500) )
-list.append( items('dator', 25 000) )
-list.append( items('jet', 350 000 000) )
+list.append( items('banan', 1))
+list.append( items('äpple', 2))
+list.append( items('godisbit', 5))
+list.append( items('monster', 20))
+list.append( items('köttbullar', 45))
+list.append( items('oxfile', 60))
+list.append( items('öl', 70))
+list.append( items('strumpor', 100))
+list.append( items('kallingar', 120))
+list.append( items('huvtröja', 400))
+list.append( items('jacka', 1000))
+list.append( items('bilmotor', 100000))
+list.append( items('båtmotor', 200000))
+list.append( items('dörr', 1500))
+list.append( items('allhjul',800))
+list.append( items('skärm', 3500))
+list.append( items('dator', 25000))
+list.append( items('jet', 350000000))
 
-items=[] 
+
+
+input("Welcome to the shop, would you like to enter?(yes/no) ")
+if "yes":
+    print("Welcome!")
+    money = input("How much money do you have on you right now?(number only) ")
+    print("Your current balance is: ", money)
+else:
+    print("Have a good day!")
+
+input("Here are the things you can afford. Is there anything you're looking to buy?(answer with the name of the item you want to buy): ")
+
+for i in range(0,len(l)):
+    if(list[i]<money):
+        print(list[i],end=" ")
+print(items) 
 
 
 
-"""
-Main()
+        
+
+
+
--- test.py
+++ test.py
@@ -1,28 +1,5 @@
-# Python3 code here creating class
-class items: 
-    def __init__(self, name, price): 
-        self.name = name 
-        self.roll = price
-   
-# creating list       
-list = [] 
-  
-# appending instances to list 
-list.append( items('banan', 0) )
-list.append( items('äpple', 0) )
-list.append( items('godisbit', 0) )
-list.append( items('monster energy', 0) )
-list.append( items('köttbullar', 0) )
-list.append( items('vegetarisk blodig nymördad oxfile', 0) )
-list.append( items('x', 0) )
-list.append( items('x', 0) )
-list.append( items('x', 0) )
-list.append( items('skärm', 0) )
-list.append( items('dator', 0) )
-list.append( items('jet', 0) )
-  
-for obj in list:
-    print( obj.name, obj.price, sep =' ' )
-  
-# We can also access instances attributes
-# as list[0].name, list[0].roll and so on.
+l=list(map(int,input("Enter array elements:").split(" ")))
+e=int(input("Enter a number:"))
+for i in range(0,len(l)):
+    if(l[i]<e):
+        print(l[i],end=" ")
